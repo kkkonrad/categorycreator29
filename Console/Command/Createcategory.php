774fe13c9ad0434c28a8cd604e9fc0188340f8d7
virtual-user <vu2034@kg.mhost.eu>
2023-06-16 10:02:27 +0200

    Modifications in Console command class

--- Console/Command/Createcategory.php
+++ Console/Command/Createcategory.php
@@ -1,6 +1,4 @@
 <?php
-declare(strict_types=1);
-
 namespace Kkkonrad\Categorycreator29\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
@@ -15,6 +13,61 @@ class Createcategory extends Command
 
     const NAME_ARGUMENT = "name";
     const NAME_OPTION = "option";
+
+    public function __construct(
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Catalog\Model\CategoryFactory $categoryFactory,
+        \Magento\Framework\Registry $registry
+    ) {
+        $this->_storeManager = $storeManager;
+        $this->_categoryFactory = $categoryFactory;
+        $this->_registry = $registry;
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName("kkkonrad:createcategory");
+        $this->setDescription("Create new Magento 2 category from console command");
+        $this->setDefinition([
+            new InputArgument(self::NAME_ARGUMENT, InputArgument::OPTIONAL, "Name"),
+            new InputOption(self::NAME_OPTION, "-d", InputOption::VALUE_NONE, "Option functionality")
+        ]);
+        parent::configure();
+    }
+
+    protected function createCategory($catName, $option)
+    {
+         $parentId = $this->_storeManager->getStore()->getRootCategoryId(); 
+         $parentCategory = $this->_categoryFactory->create()->load($parentId);
+ 
+         $category = $this->_categoryFactory->create();
+         $cate = $category->getCollection()
+             ->addAttributeToFilter('name', $catName)
+             ->getFirstItem();
+ 
+         if (!$cate->getId()) {
+             $cleanCatName = trim(preg_replace('/ +/', '', preg_replace('/[^A-Za-z0-9 ]/', '', urldecode(html_entity_decode(strip_tags($catName))))));
+             $category->setPath($parentCategory->getPath())
+                 ->setParentId($parentId)
+                 ->setName($catName)
+                 ->setUrlKey($cleanCatName)
+                 ->setIsActive(true);
+             $category->save();
+             return "Category Id: " . $category->getId() . " with name: '" . $catName . "' created.";
+         } else {
+            if($option) {
+                $this->_registry->register("isSecureArea", true);
+                $cate->delete();
+                return "Category with name: '" . $catName . "' has been deleted.";
+            } else {
+                return "Category with name: '" . $catName . "' already exists.";
+            }
+         }      
+    }
 
     /**
      * {@inheritdoc}
@@ -31,47 +84,8 @@ class Createcategory extends Command
             $output->writeln("e.q.: bin/magento kkkonrad:createcategory CatName");
             exit;
         }
-
-        // get the current stores root category
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $storeManager = $objectManager->get('\Magento\Store\Model\StoreManagerInterface');
-        $categoryFactory = $objectManager->get('\Magento\Catalog\Model\CategoryFactory');
+        $output->writeln($this->createCategory($catName, $option));
         
-        $parentId = $storeManager->getStore()->getRootCategoryId();
-
-        $parentCategory = $categoryFactory->create()->load($parentId);
-
-        $category = $categoryFactory->create();
-        $cate = $category->getCollection()
-            ->addAttributeToFilter('name', $catName)
-            ->getFirstItem();
-
-        if (!$cate->getId()) {
-            $cleanCatName = trim(preg_replace('/ +/', '', preg_replace('/[^A-Za-z0-9 ]/', '', urldecode(html_entity_decode(strip_tags($catName))))));
-            $category->setPath($parentCategory->getPath())
-                ->setParentId($parentId)
-                ->setName($catName)
-                ->setUrlKey($cleanCatName)
-                ->setIsActive(true);
-            $category->save();
-            $output->writeln("Category Id: " . $category->getId() . " with name: '" . $catName . "' created.");
-        } else {
-            $output->writeln("Category with name: '" . $catName . "' already exists.");
-        }       
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function configure()
-    {
-        $this->setName("kkkonrad:createcategory");
-        $this->setDescription("Create new Magento 2 category from console command");
-        $this->setDefinition([
-            new InputArgument(self::NAME_ARGUMENT, InputArgument::OPTIONAL, "Name"),
-            new InputOption(self::NAME_OPTION, "-a", InputOption::VALUE_NONE, "Option functionality")
-        ]);
-        parent::configure();
-    }
 }
-
