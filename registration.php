1539dd45965aeea943f7dc1dc85a136f04d9c08b
kkkonrad <konrad1983@gmail.com>
2023-06-16 12:00:42 +0200

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,5 +1,6 @@
 <?php
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Kkkonrad_Categorycreator29', __DIR__);
-
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Kkkonrad_Categorycreator29',
+    __DIR__
+);
